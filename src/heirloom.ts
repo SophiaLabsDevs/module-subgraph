cb25abe261a865bc1da23a34027e262910c49341
c0rdeiro <francisco.cordeiro.business@gmail.com>
2024-06-06 13:41:44 +0100

    WIP events

--- src/heirloom.ts
+++ src/heirloom.ts
@@ -3,91 +3,66 @@ import {
   ModuleCanceled as ModuleCanceledEvent,
   ModuleClaimed as ModuleClaimedEvent,
   ModuleReset as ModuleResetEvent,
-  ModuleSet as ModuleSetEvent
-} from "../generated/Heirloom/Heirloom"
-import {
-  ModuleBenefiaryReplaced,
-  ModuleCanceled,
-  ModuleClaimed,
-  ModuleReset,
-  ModuleSet
-} from "../generated/schema"
+  ModuleSet as ModuleSetEvent,
+} from '../generated/Heirloom/Heirloom'
+import { Module } from '../generated/schema'
 
 export function handleModuleBenefiaryReplaced(
   event: ModuleBenefiaryReplacedEvent
 ): void {
-  let entity = new ModuleBenefiaryReplaced(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._densityModule = event.params._densityModule
-  entity._moduleId = event.params._moduleId
-  entity._previousBeneficiary = event.params._previousBeneficiary
-  entity._newBeneficiary = event.params._newBeneficiary
+  const id = event.params._moduleId.toString() + event.params._densityModule.toString() //unique identifier -> union of moduleId and densityModule address
+  const module = Module.load(id) //locate entry with matching Id
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  if (module) {
+    module.beneficiary = event.params._newBeneficiary
+    module.save()
+  }
 }
 
 export function handleModuleCanceled(event: ModuleCanceledEvent): void {
-  let entity = new ModuleCanceled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._densityModule = event.params._densityModule
-  entity._moduleId = event.params._moduleId
-  entity._timer = event.params._timer
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+ 
 }
 
 export function handleModuleClaimed(event: ModuleClaimedEvent): void {
-  let entity = new ModuleClaimed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._densityModule = event.params._densityModule
-  entity._moduleId = event.params._moduleId
-  entity._beneficiary = event.params._beneficiary
+  const id = event.params._moduleId.toString() + event.params._densityModule.toString() //unique identifier -> union of moduleId and densityModule address
+  const module = Module.load(id) //locate entry with matching Id
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  if (module) {
+    module.claimed = true
+    module.save()
+  }
 }
 
 export function handleModuleReset(event: ModuleResetEvent): void {
-  let entity = new ModuleReset(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._densityModule = event.params._densityModule
-  entity._moduleId = event.params._moduleId
-  entity._timer = event.params._timer
+  const id = event.params._moduleId.toString() + event.params._densityModule.toString() //unique identifier -> union of moduleId and densityModule address
+  const module = Module.load(id) //locate entry with matching Id
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  if (module) {
+    module.timer = event.params._timer
+    module.save()
+  }
 }
 
 export function handleModuleSet(event: ModuleSetEvent): void {
-  let entity = new ModuleSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._densityModule = event.params._densityModule
-  entity._moduleId = event.params._moduleId
-  entity._beneficiary = event.params._beneficiary
-  entity._timer = event.params._timer
+  const id = event.params._moduleId.toString() + event.params._densityModule.toString()
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  const module = Module.load(id) //locate entry with matching Id
 
-  entity.save()
+  if (!module) {
+    const entity = new Module(id)
+    entity.densityModule = event.params._densityModule
+    entity.moduleId = event.params._moduleId
+    entity.beneficiary = event.params._beneficiary
+    entity.timer = event.params._timer
+    entity.claimed = false
+
+    entity.save()
+  } else {
+    module.densityModule = event.params._densityModule
+    module.moduleId = event.params._moduleId
+    module.beneficiary = event.params._beneficiary
+    module.timer = event.params._timer
+
+    module.save()
+  }
 }
